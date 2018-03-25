0ce8be1f0dafcc108beb18c30cf18175fe9030a9
Taiji Tsuchiya <tuty0630@gmail.com>
2018-03-24 23:48:45 -0700

    add file

--- tests/test_router.py
+++ tests/test_router.py
@@ -2,9 +2,17 @@ import unittest
 from router import Router
 
 class TestRouter(unittest.TestCase):
-    def test_test(self):
-        expected    = 1
-        actual      = 1
+    def setUp(self):
+        self.router_junos = Router(
+            hostname    = 'vsrx1',
+            os          = '12.1X47-D15.4',
+            ipaddress   = '192.168.33.3',
+            username    = 'user1',
+            password    = 'password1')
+
+    def test_get_hostname(self):
+        expected    = self.router_junos.hostname
+        actual      = self.router_junos.get_hostname()
         self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
